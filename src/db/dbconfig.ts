7e974f623419354d2eb712b53640427cfe03ea60
tomaslachmann <tomaslachmann@gmail.com>
2021-11-24 14:29:06 +0100

    test

---
+++ src/db/dbconfig.ts
@@ -0,0 +1,10 @@
+
+import { Client } from 'pg';
+
+export const client = new Client({
+    user: 'tomaslachmanngmailcom_2093',
+    host: 'store5.rosti.cz',
+    database: 'tomaslachmanngmailcom_2093',
+    password: 'Lachty25051995',
+    port: 5432,
+});
